6c9ece29c43b0d026dfba2591c21c858611d8069
Shubhamghude808 <112695994+Shubhamghude808@users.noreply.github.com>
2026-05-10 23:20:58 +0530

    SupaBase initial setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,38 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
+import { useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import { supabase } from '../lib/supabase';
 
 export default function Login() {
+
+  useEffect(() => {
+    const test = async () => {
+      const { data, error } = await supabase
+        .from('schools')
+        .select('*');
+
+      console.log('Supabase connected:', data, error);
+    };
+
+    test();
+  }, []);
+
   return (
     <View style={styles.container}>
 
-      {/* Logo */}
       <View style={styles.logoBox}>
         <Text style={styles.logo}>📘</Text>
       </View>
 
       <Text style={styles.title}>Teacher Portal</Text>
 
-      {/* Inputs */}
       <TextInput
         placeholder="Teacher Name"
         style={styles.input}
@@ -26,7 +46,6 @@ export default function Login() {
         placeholderTextColor="#999"
       />
 
-      {/* Button */}
       <TouchableOpacity
         style={styles.button}
         onPress={() => router.push('/batch')}
@@ -45,6 +64,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20
   },
+
   logoBox: {
     backgroundColor: '#3b82f6',
     alignSelf: 'center',
@@ -52,28 +72,33 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginBottom: 10
   },
+
   logo: {
     fontSize: 30,
     color: 'white'
   },
+
   title: {
     textAlign: 'center',
     fontSize: 20,
     fontWeight: '600',
     marginBottom: 30
   },
+
   input: {
     backgroundColor: '#e5e7eb',
     padding: 15,
     borderRadius: 12,
     marginBottom: 15
   },
+
   button: {
     backgroundColor: '#3b82f6',
     padding: 15,
     borderRadius: 12,
     marginTop: 10
   },
+
   buttonText: {
     color: 'white',
     textAlign: 'center',
